3d4603bda960289d77dbff63f5aa62d3af940f57
Jealous Matsikachando <pasej5@gmail.com>
2024-08-03 01:25:33 +0200

    change to ubuntu ownership

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -14,7 +14,11 @@ python manage.py collectstatic --noinput
 
 # Adjust permissions and ownership of the SQLite database
 sudo chmod 664 db.sqlite3
-sudo chown www-data:www-data db.sqlite3
+sudo chown ubuntu:www-data db.sqlite3
+
+# Ensure the project directory has the correct permissions
+sudo chmod 775 /home/ubuntu/peace_online
+sudo chown ubuntu:www-data /home/ubuntu/peace_online
 
 # Reload and restart services
 sudo systemctl daemon-reload
