6e874cb85cc6c7f568ea3d7ef6aec6e0eada87bd
Jealous Matsikachando <pasej5@gmail.com>
2024-08-03 01:37:10 +0200

    new gunicorn

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
-sudo cp /home/ubuntu/peace_online/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/peace_online/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
+# Copy the Gunicorn service files
+sudo cp /home/ubuntu/peace_online/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/peace_online/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
+
+# Adjust permissions and ownership of the SQLite database
+cd /home/ubuntu/peace_online
+sudo chmod 664 db.sqlite3
+sudo chown www-data:www-data db.sqlite3
+
+# Start and enable the Gunicorn service
 sudo systemctl start gunicorn.service
 sudo systemctl enable gunicorn.service
