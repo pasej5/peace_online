2a072af442b205b0b59705dbe7fb2b348d607254
Jealous Matsikachando <pasej5@gmail.com>
2024-08-03 11:50:02 +0200

    new database location on the server

--- peace_online/settings.py
+++ peace_online/settings.py
@@ -104,7 +104,8 @@ WSGI_APPLICATION = 'peace_online.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/var/lib/peace_online', 'db.sqlite3'),
+
     }
 }
 
