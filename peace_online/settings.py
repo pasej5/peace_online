a6c72252137a2771326c8ef22d17cf10c3b489c8
Jealous Matsikachando <pasej5@gmail.com>
2024-08-12 05:56:35 +0200

    staticfiles

--- peace_online/settings.py
+++ peace_online/settings.py
@@ -157,6 +157,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('/var', 'www', 'media')
 
